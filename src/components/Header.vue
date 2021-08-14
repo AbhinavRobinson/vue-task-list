3e10d4113e80d00a7e3b7ac93c7dfcde26efbb96
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-08-14 15:33:06 +0530

    (feat): add header

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>Task Tracker</header>
+</template>
+
+<script lang="ts">
+export default { name: "Header" };
+</script>
+
+<style lang="less" scoped>
+header {
+  font-weight: bold;
+  font-size: larger;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="container">
+      <Header />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Header,
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="less" scoped>
+@padding: 20px;
+.home {
+  display: grid;
+  place-items: center;
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: calc(400px - @padding * 2);
+    padding: @padding;
+    border: 1px solid #e5e5e5;
+    border-radius: calc(@padding / 2);
+    transition-duration: 300ms;
+    &:hover {
+      box-shadow: 0 5px 10px rgb(235, 235, 235);
+    }
+  }
+}
+</style>
