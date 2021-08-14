f1d942316f577ce69b4f0b063363028ee19f8293
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-08-14 15:45:52 +0530

    (feat): add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    My first vue app.<br />
+    Made with <span>&lt;3</span> by
+    <a href="http://github.com/abhinavrobinson" class="default"
+      >Abhinav Robinson</a
+    >
   </div>
 </template>
+
+<style lang="less" scoped>
+span {
+  color: #42b983;
+  font-weight: bold;
+}
+a {
+  font-weight: bold;
+  &:visited {
+    color: #42b983;
+  }
+  &:active {
+    color: #217750;
+  }
+}
+</style>
