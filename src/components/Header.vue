23a758236231d518aacd8223cf02ec933cc82b42
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-08-14 21:07:38 +0530

    (feat): add store

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,51 @@
 <template>
-  <header>Task Tracker</header>
+  <div class="bar">
+    <header>Task Tracker</header>
+    <button>Add Task</button>
+  </div>
 </template>
 
 <script lang="ts">
-export default { name: "Header" };
+import { Options, Vue } from "vue-class-component";
+@Options({
+  computed: {
+    tasks: function () {
+      return this.$store.state.tasks;
+    },
+  },
+})
+export default class Header extends Vue {}
 </script>
 
 <style lang="less" scoped>
-header {
-  font-weight: bold;
-  font-size: larger;
+@import "../styles.less";
+.bar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  header {
+    font-weight: bold;
+    font-size: larger;
+  }
+  button {
+    background-color: white;
+    border: 1px solid @dark;
+    border-radius: @padding / 4;
+    padding: @padding / 2;
+    font-weight: bold;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition-duration: 150ms;
+    &:hover {
+      border: 1px solid @primary;
+      background-color: @primary;
+      color: white;
+    }
+    &:active {
+      transform: translateY(2px) scale(0.98);
+    }
+  }
 }
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,18 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    taskCount: 0,
+    tasks: [{ title: "INITIAL", time: "8:00PM" }] as {
+      [key: string]: string;
+    }[],
+  },
+  mutations: {
+    addTask(state, task: string) {
+      state.tasks.push({ title: task, time: "8:00PM" });
+      state.taskCount++;
+    },
+  },
   actions: {},
   modules: {},
 });
